0df9cdc67ac14415d31c6c3023386389b9fff866
Abdul Rafay <abdulrafey98490@gmail.com>
2024-07-14 17:39:06 +0530

    Initial Commit

---
+++ tailwind.config.js
@@ -0,0 +1,23 @@
+module.exports = {
+  content: ["./*.html"],
+  theme: {
+    screens: {
+      sm: "480px",
+      md: "955px",
+      lg: "1020px",
+      xl: "1440px",
+    },
+    extend: {
+      colors: {
+        darkpurple: "#24053E",
+        lightpurple: "#584D62",
+        greenish: "#44FFA1",
+        ghost: "#FCF8FF",
+      },
+      fontFamily: {
+        serif: ["Fraunces", "sans-serif"],
+      },
+    },
+  },
+  plugins: [],
+};
